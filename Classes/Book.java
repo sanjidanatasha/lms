495421375e9cb8377ca820e014836d48df4a03d2
sanjidanatasha <sanjidanatasha09@gmail.com>
2025-09-15 20:37:04 +0600

    Update Book.java

--- Classes/Book.java
+++ Classes/Book.java
@@ -1,21 +1,19 @@
 package Classes;
 
 public class Book {
-    private String bookId;
-    private String bookName;
-    private String bookAuthor;
-    private String bookType;
-    private int bookCopy;
-    private String image;
-    private static int bookCounter = 0;
 
-    public Book(String bookId, String bookName, String bookAuthor, String bookType, int bookCopy, String image) {
+    private String bookNamee;  
+    private String bookAuthor;
+    private String bookId;
+    private String bookType;
+         private static int bookCounter = 0;
+
+    public Book(String bookId, String bookName, String bookAuthor, String bookType) {
         this.bookName = bookName;
         this.bookAuthor = bookAuthor;
         this.bookId = bookId;
         this.bookType = bookType;
-        this.bookCopy = bookCopy;
-        this.image = image;
+    
         bookCounter++;
     }
 
@@ -35,13 +33,6 @@ public class Book {
         this.bookType = bookType;
     }
 
-    public void setBookCopy(int bookCopy) {
-        this.bookCopy = bookCopy;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
 
     public String getBookName() {
         return bookName;
@@ -55,36 +46,27 @@ public class Book {
         return bookId;
     }
 
-    public int getBookCopy() {
-        return bookCopy;
-    }
-
     public String getBookType() {
         return bookType;
     }
 
-    public String getImage() {
-        return image;
-    }
-
+    
     public void showBookInfo() {
         System.out.println("Book Name : " + bookName);
         System.out.println("Book Author : " + bookAuthor);
         System.out.println("Book Id : " + bookId);
         System.out.println("Book Type : " + bookType);
-        System.out.println("Number of Book Copy : " + bookCopy);
+        
     }
 
-    public void addBookCopy(int x) {
-        bookCopy += x;
-    }
+    
 
     public static void totalNumberOfUniqueBooks() {
         System.out.println("Total Number Of Unique Books : " + bookCounter);
     }
 
     public String getBookInfoAsString() {
-        return bookId + ";" + bookName + ";" + bookAuthor + ";" + bookType + ";" + bookCopy + ";" + image;
+        return bookId + ";" + bookName + ";" + bookAuthor + ";" + bookType;
     }
 
 }

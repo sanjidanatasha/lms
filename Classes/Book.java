56ebdcfe502f190a2bcc612b3d1cff4a9ba6c3c4
sanjidanatasha <sanjidanatasha09@gmail.com>
2025-09-15 20:13:26 +0600

    update BOOK.java
    Co-Authored-By: novaanjum30-cell <232095960+novaanjum30-cell@users.noreply.github.com>
    Co-Authored-By: Abid Mahmood Akash <61755558+AMAKASH@users.noreply.github.com>

--- Classes/Book.java
+++ Classes/Book.java
@@ -1,39 +1,90 @@
 package Classes;
 
 public class Book {
-    private static int idCounter = 0;
-    private int id;
-    private String name;
-    private String author;
+    private String bookId;
+    private String bookName;
+    private String bookAuthor;
+    private String bookType;
+    private int bookCopy;
+    private String image;
+    private static int bookCounter = 0;
 
-    public Book(String name, String author) {
-        this.name = name;
-        this.author = author;
-        this.id = ++Book.idCounter;
+    public Book(String bookId, String bookName, String bookAuthor, String bookType, int bookCopy, String image) {
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.bookId = bookId;
+        this.bookType = bookType;
+        this.bookCopy = bookCopy;
+        this.image = image;
+        bookCounter++;
     }
 
-    // public int getID(){
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
 
-    // }
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
 
-    // public String getName(){
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
 
-    // }
+    public void setBookType(String bookType) {
+        this.bookType = bookType;
+    }
 
-    // public String getAuthor(){
+    public void setBookCopy(int bookCopy) {
+        this.bookCopy = bookCopy;
+    }
 
-    // }
+    public void setImage(String image) {
+        this.image = image;
+    }
 
-    // public void setName(String name){
+    public String getBookName() {
+        return bookName;
+    }
 
-    // }
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
 
-    // public void setAuthor(String contact){
+    public String getBookId() {
+        return bookId;
+    }
 
-    // }
+    public int getBookCopy() {
+        return bookCopy;
+    }
+
+    public String getBookType() {
+        return bookType;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void showBookInfo() {
+        System.out.println("Book Name : " + bookName);
+        System.out.println("Book Author : " + bookAuthor);
+        System.out.println("Book Id : " + bookId);
+        System.out.println("Book Type : " + bookType);
+        System.out.println("Number of Book Copy : " + bookCopy);
+    }
+
+    public void addBookCopy(int x) {
+        bookCopy += x;
+    }
+
+    public static void totalNumberOfUniqueBooks() {
+        System.out.println("Total Number Of Unique Books : " + bookCounter);
+    }
 
     public String getBookInfoAsString() {
-        return name + ";" + author;
+        return bookId + ";" + bookName + ";" + bookAuthor + ";" + bookType + ";" + bookCopy + ";" + image;
     }
 
 }
